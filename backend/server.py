fabbb7a7fcef86c8ec1680cdcea1a9080feda8c6
paulojfsf-creator <paulo.jfsf@gmail.com>
2026-04-15 08:39:01 +0100

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -28,6 +28,7 @@ cloudinary.config(
     cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
     api_key=os.environ.get("CLOUDINARY_API_KEY"),
     api_secret=os.environ.get("CLOUDINARY_API_SECRET")
+)
 
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
